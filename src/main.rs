288db74267f414667249e219cfd149e36a1dae21
AlexeyMashkovichUniWork <st142037@student.spbu.ru>
2025-11-03 16:24:53 +0300

    Add.. the whole program, it's quite simple

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
+use std::fs::File;
+use std::io;
+use std::io::Read;
 
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut buffer)?;
+
+    let file_open_result = File::open(buffer);
+    let mut file;
+    match file_open_result {
+        Err(_) => {
+            println!("failure");
+            return Ok(());
+        }
+        Ok(result) => {
+            file = result;
+        }
+    };
+    let mut buf: Vec<_> = Vec::new();
+    match file.read(&mut buf) {
+        Err(_) => {
+            println!("failure");
+        }
+        Ok(_) => {
+            println!("success");
+        }
+    }
+
+    Ok(())
 }
